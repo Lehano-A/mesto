25bf382cf150e11301b875b205bf0da785820b2a
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2021-03-19 18:28:26 +0300

    Добавить скрипт на изменение цвета кнопки 'Like' на карточке

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,54 @@
 //Кнопка "Редактировать профиль"
 
+
 //Popup открывается
 let editButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 
 function popupOpen() {
-    let popupRemoveClass = popup.classList.remove('popup_close');
+    popup.classList.remove('popup_close');
 }
 
 editButton.addEventListener('click', popupOpen);
 
 
+
 //Popup закрывается по клику на кнопку закрытия
-let popupButtonClose = document.querySelector('.popup__button-close')
+let popupButtonClose = document.querySelector('.popup__button-close');
 
 function popupClose() {
-    let popupAddClass = popup.classList.add('popup_close')
+    popup.classList.add('popup_close');
 }
 
 popupButtonClose.addEventListener('click', popupClose);
 
 
+
 //Popup закрывается по клику на overlay
-let popupOverlay = document.querySelector('.popup__overlay')
+let popupOverlay = document.querySelector('.popup__overlay');
 
 popupOverlay.addEventListener('click', popupClose);
+
+
+
+///////////////////////////////////////////////////
+
+
+
+//Кнопка "like"
+
+//Сердце меняет цвет
+let buttonsLike = document.querySelectorAll('.element__button-like');
+
+
+for (i = 0; i < buttonsLike.length; i++) {
+    let actualInactive = buttonsLike[i];
+
+        function removeClass() {
+            actualInactive.classList.toggle('element__button-like_inactive');
+          }
+        actualInactive.addEventListener('click', removeClass);
+        
+}
+
 

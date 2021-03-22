924725bbbd4b0ce00169eb2de3dc94e243c913d5
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2021-03-23 00:14:19 +0300

    Изменены отступы у элементов profile, header_logo. Небольшая правка в скриптах

--- scripts/index.js
+++ scripts/index.js
@@ -10,16 +10,16 @@ let popupButtonClose = document.querySelector('.popup__button-close');
 //Popup сохраняет информацию из inputs
 let buttonSave = document.querySelector('.popup__button-save');
 let formElement = document.querySelector('.popup__form');
-let nameInput = formElement.querySelector('.popup__input[id="popup_name"]')
-let statusInput = formElement.querySelector('.popup__input[id="popup_status"]')
+let nameInput = formElement.querySelector('#popup_name')
+let statusInput = formElement.querySelector('#popup_status')
 let profileName = document.querySelector('.profile__name'); //Жак-Ив Кусто
 let profileStatus = document.querySelector('.profile__status'); //Исследователь океана
 
 
 //Popup открывается
 function popupOpen() {
-  nameInput.textContent = profileName.value;
-  statusInput.textContent = profileStatus.value;
+  nameInput.value = profileName.textContent;
+  statusInput.value = profileStatus.textContent;
   popup.classList.add('popup_opened');
 }
 

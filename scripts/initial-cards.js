7dc60f85319439b611fd5ab557573ac7b1f0fd67
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2021-04-03 01:18:12 +0300

    Внесены мелкие исправления и доработки

---
+++ scripts/initial-cards.js
@@ -0,0 +1,30 @@
+// Массив изначально подгружающихся карточек
+
+const initialCards = [
+  {
+    name: 'Планета Земля',
+    link: './images/milky-way-planet-earth.jpg'
+  },
+  {
+    name: 'Судак, Крым',
+    link: './images/sudak-crimea.jpg'
+  },
+  {
+    name: 'Моаб, США',
+    link: './images/moab-usa.jpg'
+  },
+  {
+    name: 'Чжанъе, Китай',
+    link: './images/zhangye-china.jpg'
+  },
+  {
+    name: 'Онтарио, Канада',
+    link: './images/ontario-canada.jpg'
+  },
+  {
+    name: 'Мон-Сен-Мишель, Франция',
+    link: './images/mont-saint-michel-france.jpg'
+  }
+];
+
+

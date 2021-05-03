c427e23bcf23d0111052507bd34458250790b87e
Lehano-A <48220644+Lehano-A@users.noreply.github.com>
2021-05-03 22:39:36 +0300

    Рефакторинг

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -2,30 +2,22 @@
 export default class FormValidator {
 
   constructor(enableValidationConfig, formElement) {
-    this._formSelector = enableValidationConfig.formSelector; // ФОРМА
-    this._inputSelector = enableValidationConfig.inputSelector; // ПОЛЕ
-    this._submitButtonSelector = enableValidationConfig.submitButtonSelector; // КНОПКА "ОТПРАВКИ ДАННЫХ"
-    this._inactiveButtonClass = enableValidationConfig.inactiveButtonClass; // КНОПКА "ОТПРАВКА ДАННЫХ" НЕАКТИВНА
+    this._formSelector = enableValidationConfig.formSelector; // СЕЛЕКТОР ФОРМЫ
+    this._inputSelector = enableValidationConfig.inputSelector; // СЕЛЕКТОР ПОЛЯ В ФОРМЕ
+    this._submitButtonSelector = enableValidationConfig.submitButtonSelector; // СЕЛЕКТОР КНОПКИ "ОТПРАВКИ ДАННЫХ"
+    this._inactiveButtonClass = enableValidationConfig.inactiveButtonClass; // СЕЛЕКТОРА КНОПКИ "ОТПРАВКА ДАННЫХ" НЕАКТИВНА
     this._inputErrorClass = enableValidationConfig.inputErrorClass; // СТИЛЬ ПОЛЯ ВО ВРЕМЯ НЕВАЛИДНОСТИ
-    this._errorClass = enableValidationConfig.errorClass; // СПАН С ОШИБКОЙ В ПОЛЕ АКТИВЕН
-    this._spanErrorActive = enableValidationConfig.spanErrorActive; // СПАН С ОШИБКОЙ АКТИВЕН
-    this._formElement = formElement; // ССЫЛКА ВХОДЯЩЕЙ ФОРМЫ
-  }
-
-
-  // ПЕРЕСЧЁТ ВСЕХ ФОРМ
-  _recalculatePrimaryValidityAllForms() {
-    this._allFormsElements = Array.form(document.querySelectorAll(this._formSelector)); // МАССИВ ИЗ ФОРМ
-    this._allFormsElements.forEach((form) => {
-      this._setEventListeners(form); // ОТПРАВЛЯЕМ КАЖДУЮ НАЙДЕННУЮ ФОРМУ НА УСТАНОВКУ СЛУШАТЕЛЕЙ
-    });
+    this._errorClass = enableValidationConfig.errorClass; // СЕЛЕКТОРА СПАНА С ОШИБКОЙ В ПОЛЕ АКТИВЕН
+    this._spanErrorActive = enableValidationConfig.spanErrorActive; // СЕЛЕКТОР СПАНА С ОШИБКОЙ АКТИВЕН
+    this._formElement = formElement; // ЭЛЕМЕНТ ПРИНЯТОЙ ФОРМЫ
+    this._submitButton = formElement.querySelector(enableValidationConfig.submitButtonSelector); // КНОПКА "ОТПРАВКА ДАННЫХ" ПРИНЯТОЙ ФОРМЫ
+    this._inputList = Array.from(formElement.querySelectorAll(enableValidationConfig.inputSelector)); // МАССИВ ПОЛЕЙ ПРИНЯТОЙ ФОРМЫ
   }
 
 
   // ПОЛУЧЕНИЕ ФОРМЫ ИЗ index.js
   _receiveFormElement() {
     this._setEventListeners(this._formElement); // ОТПРАВЛЯЕМ СТАВИТЬ СЛУШАТЕЛЬ НА ФОРМУ
-    this._hasActiveSpanError(this._formElement); // ОТПРАВЛЯЕМ НА ПРОВЕРКУ НАЛИЧИЯ АКТИВНЫХ ОШИБОК ПОЛЕЙ
   }
 
 
@@ -35,124 +27,69 @@ export default class FormValidator {
   }
 
 
-  // МЕТОД ПРОВЕРКИ НАЛИЧИЯ АКТИВНЫХ ОШИБОК ПОЛЕЙ ВО ВРЕМЯ ЗАКРЫТИЯ ФОРМЫ
-  _hasActiveSpanError(formElement) {
-
-    if (formElement.querySelector(`.${this._spanErrorActive}`)) {
-
-      this._arrayFormElement = Array.from(formElement);
-      this._arrayFormElement.forEach((item) => {
-
-        if (item.classList.contains(this._inputErrorClass)) {
-          this._hideInputError(formElement, item);
-        }
-      });
-    }
-  }
-
-
-  // ОБРАБОТЧИК СЛУШАТЕЛЯ SUBMIT
-  _handlePreventDefault(evt) {
-    evt.preventDefault();
-  }
-
-
-  // ОБРАБОТЧИК СЛУШАТЕЛЯ INPUT
-  _handleInputListener(formElement, eachInputElement) {
-    this._checkAtValid(formElement, eachInputElement); // ПРИ ЛЮБОМ ВВОДЕ/УДАЛЕНИИ СИМВОЛОВ - ОТПРАВЛЯЕМ НА ВАЛИДАЦИЮ ФОРМУ И ПОЛЕ
-    this._toggleButtonDesign(this._inputsFormElements, this._buttonFormSubmitElement); // ОТПРАВЛЯЕМ НА ВАЛИДАЦИЮ СРАЗУ ВСЕ ПОЛЯ И КНОПКУ "ОТПРАВКИ ДАННЫХ"
-  }
-
-
   // СТАВИМ СЛУШАТЕЛЕЙ НА ФОРМЫ И ПОЛЯ
-  _setEventListeners(formElement) {
+  _setEventListeners() {
 
     // СЛУШАТЕЛЬ SUBMIT
-    formElement.addEventListener('submit', (evt) => { // НА КАЖДУЮ ФОРМУ СТАВИМ 'SUBMIT'
-      this._eventFormElement = evt;
-      this._handlePreventDefault(this._eventFormElement);
+    this._formElement.addEventListener('submit', (evt) => { // НА КАЖДУЮ ФОРМУ СТАВИМ 'SUBMIT'
+      evt.preventDefault();
     });
 
-    // ПОЛУЧЕННУЮ ФОРМУ ОТПРАВЛЯЕМ НА СОЗДАНИЕ МАССИВА ПОЛЕЙ С ДАЛЬНЕЙШЕЙ ОТПРАВКОЙ НА ПЕРВИЧНУЮ ВАЛИДАЦИЮ
-    this._createArrayInputsElements(formElement);
-
     // СЛУШАТЕЛЬ INPUT
-    this._inputsFormElements.forEach((eachInputElement) => { // ПЕРЕСЧИТЫВАЕМ ВСЕ ПОЛЯ В ФОРМЕ
+    this._inputList.forEach((eachInputElement) => { // ПЕРЕСЧИТЫВАЕМ ВСЕ ПОЛЯ В ФОРМЕ
       eachInputElement.addEventListener('input', () => { // КАЖДОМУ ПОЛЮ ВЕШАЕМ 'INPUT'
-        this._handleInputListener(formElement, eachInputElement);
+
+        if (!eachInputElement.validity.valid) {
+          this._spanInputError = this._formElement.querySelector(`.${eachInputElement.id}-error`);
+          eachInputElement.classList.add(this._inputErrorClass);
+          this._spanInputError.classList.add(this._spanErrorActive);
+          this._spanInputError.textContent = eachInputElement.validationMessage;
+          this._toggleButtonDesign();
+        } else {
+
+          this._spanInputError = this._formElement.querySelector(`.${eachInputElement.id}-error`);
+          eachInputElement.classList.remove(this._inputErrorClass);
+          this._spanInputError.classList.remove(this._spanErrorActive);
+          this._spanInputError.textContent = '';
+          this._toggleButtonDesign();
+        }
       });
     });
   }
 
 
-  // СОЗДАНИЕ МАССИВА ПОЛЕЙ ИЗ ПРИНЯТОЙ ФОРМЫ С ОТПРАВКОЙ НА ПЕРВИЧНУЮ ВАЛИДАЦИЮ
-  _createArrayInputsElements(form) {
-    this._inputsFormElements = Array.from(form.querySelectorAll(this._inputSelector)); // ДЕЛАЕМ МАССИВ ПОЛЕЙ ИЗ ОТДЕЛЬНОЙ ФОРМЫ
-    this._buttonFormSubmitElement = form.querySelector(this._submitButtonSelector); // НАХОДИМ КНОПКУ "ОТПРАВКИ ДАННЫХ" В ОТДЕЛЬНОЙ ФОРМЕ
-    this._toggleButtonDesign(this._inputsFormElements, this._buttonFormSubmitElement); // ОТПРАВЛЯЕМ СРАЗУ НА ПЕРВИЧНУЮ ВАЛИДАЦИЮ ВСЕ ПОЛЯ
-  }
+  _toggleButtonDesign() {
 
-
-  // ПРОВЕРКА ВАЛИДНОСТИ ПОЛЯ, В КОТОРОМ ВВОДЯТСЯ ДАННЫЕ +
-  // ОТПРАВКА КОМАНДЫ НА ВКЛЮЧЕНИЕ/ОТКЛЮЧЕНИЕ ОШИБКИ ВВОДА ДАННЫХ
-  _checkAtValid(form, eachInputElement) {
-    if (!eachInputElement.validity.valid) {
-      this._showInputError(form, eachInputElement); // FALSE - ПОКАЗЫВАЕМ ОШИБКУ
-
-    } else {
-
-      this._hideInputError(form, eachInputElement); // TRUE - СКРЫВАЕМ ОШИБКУ
-
-    }
-  }
-
-
-  // FALSE - ПОКАЗЫВАЕМ ОШИБКУ ВВОДА ДАННЫХ В ПОЛЕ
-  _showInputError(form, eachInputElement) {
-
-    this._spanInputError = form.querySelector(`.${eachInputElement.id}-error`);
-    eachInputElement.classList.add(this._inputErrorClass);
-    this._spanInputError.classList.add(this._spanErrorActive);
-    this._spanInputError.textContent = eachInputElement.validationMessage;
-  }
-
-
-  // TRUE - ПРЯЧЕМ ОШИБКУ ВВОДА ДАННЫХ В ПОЛЕ
-  _hideInputError(form, eachInputElement) {
-    this._spanInputError = form.querySelector(`.${eachInputElement.id}-error`);
-    eachInputElement.classList.remove(this._inputErrorClass);
-    this._spanInputError.classList.remove(this._spanErrorActive);
-    this._spanInputError.textContent = '';
-  }
-
-
-  // ИЗМЕНЕНИЕ ДИЗАЙНА КНОПКИ "ОТПРАВКИ ДАННЫХ"
-  // (ЕСЛИ ХОТЯ БЫ ОДНО ПОЛЕ НЕВАЛИДНО - КНОПКА НЕАКТИВНА)
-  // (ЕСЛИ ВСЁ ВАЛИДНО - КНОПКА АКТИВНА)
-  _toggleButtonDesign(inputsFormElements, buttonFormSubmitElement) {
-
-    this._checkInvalidInput = this._hasInvalidInput(inputsFormElements); // ОТПРАВИЛИ НА ПРОВЕРКУ ВАЛИДНОСТИ
+    this._checkInvalidInput = this._hasInvalidInput(); // ОТПРАВИЛИ НА ПРОВЕРКУ ВАЛИДНОСТИ
 
     if (this._checkInvalidInput) { // ЕСЛИ ХОТЯ БЫ ОДНО ПОЛЕ НЕВАЛИДНО
-      buttonFormSubmitElement.setAttribute('disabled', true); // ДЕАКТИВИРУЕМ КНОПКУ "ОТПРАВКИ ДАННЫХ"
-      buttonFormSubmitElement.classList.add(this._inactiveButtonClass); // МЕНЯЕМ ДИЗАЙН КНОПКИ "ОТПРАВКИ ДАННЫХ"
+      this._submitButton.setAttribute('disabled', true); // ДЕАКТИВИРУЕМ КНОПКУ "ОТПРАВКИ ДАННЫХ"
+      this._submitButton.classList.add(this._inactiveButtonClass); // МЕНЯЕМ ДИЗАЙН КНОПКИ "ОТПРАВКИ ДАННЫХ"
     } else { // ЕСЛИ ВСЕ ПОЛЯ ВАЛИДНЫ
-      buttonFormSubmitElement.removeAttribute('disabled'); // АКТИВИРУЕМ КНОПКУ "ОТПРАВКИ ДАННЫХ"
-      buttonFormSubmitElement.classList.remove(this._inactiveButtonClass); // "МЕНЯЕМ ДИЗАЙН КНОПКИ "ОТПРАВКИ ДАННЫХ"
+
+      this._submitButton.removeAttribute('disabled'); // АКТИВИРУЕМ КНОПКУ "ОТПРАВКИ ДАННЫХ"
+      this._submitButton.classList.remove(this._inactiveButtonClass); // "МЕНЯЕМ ДИЗАЙН КНОПКИ "ОТПРАВКИ ДАННЫХ"
     }
   }
 
 
   // ПРОВЕРКА ВАЛИДНОСТИ СРАЗУ ВСЕХ ПОЛЕЙ ОТДЕЛЬНОЙ ФОРМЫ
-  _hasInvalidInput(inputsFormElements) { // ПРИНИМАЕТ МАССИВ ПОЛЕЙ ФОРМЫ
-    return inputsFormElements.some((eachInputElement) => { // ОСУЩЕСТВЛЯЕТ ПРОВЕРКУ ВАЛИДНОСТИ КАЖДОГО ПОЛЯ ИЗ МАССИВА ПОЛЕЙ
+  _hasInvalidInput() { // ПРИНИМАЕТ МАССИВ ПОЛЕЙ ФОРМЫ
+
+    return this._inputList.some((eachInputElement) => { // ОСУЩЕСТВЛЯЕТ ПРОВЕРКУ ВАЛИДНОСТИ КАЖДОГО ПОЛЯ ИЗ МАССИВА ПОЛЕЙ
       return !eachInputElement.validity.valid; // ВОЗВРАЩАЕТ НЕВАЛИДНОСТЬ
     });
   }
 
+  // МЕТОД СБРАСЫВАНИЯ ОШИБОК ФОРМЫ ПЕРЕД ОТКРЫТИЕ ПОПАПА
+  resetValidation() {
+    this._inputList.forEach((inputElement) => {
+      this._spanInputError = this._formElement.querySelector(`.${inputElement.id}-error`);
+      inputElement.classList.remove(this._inputErrorClass);
+      this._spanInputError.classList.remove(this._spanErrorActive);
+      this._spanInputError.textContent = '';
+    });
+
+    this._toggleButtonDesign();
+  }
 }
-
-
-
-
-
--- scripts/close-popup.js
+++ scripts/close-popup.js
@@ -6,30 +6,11 @@ function closePopupByEsc(evt) {
   }
 }
 
-
 // ЗАКРЫВАТЕЛЬ ПОПАПА
 function closePopup(popup) { // ПРИНИМАЕТ В СЕБЯ НАЙДЕННЫЙ ОТКРЫТЫЙ ПОПАП
   document.removeEventListener('keydown', closePopupByEsc);
   popup.classList.remove('popup_opened');
 
 }
-
-
-// ЗАКРЫВАТЕЛЬ ПОПАПА ПО КЛИКУ НА ОВЕРЛЕЙ
-function closeOverlay() {
-  const allPopupsOverlay = document.querySelectorAll('.popup'); // ИЩЕМ ВСЕ ОВЕРЛЕИ ПОПАПОВ
-  allPopupsOverlay.forEach((item) => { // ДЕЛАЕМ ПЕРЕСЧЕТ ОВЕРЛЕЕВ
-
-    // MOUSEDOWN В СВЯЗИ С ТЕМ, ЧТО ЕСЛИ БУДЕТ 'CLICK', ТО ПРИ ВЫДЕЛЕНИИ ТЕКСТА В ПОЛЕ ФОРМЫ
-    // МЫШКОЙ С ПОМОЩЬЮ ЛЕВОЙ КЛАВИШИ, И ЕСЛИ ЭТУ КЛАВИШУ ОТПУСТИТЬ НА ОВЕРЛЕЕ, ТО ПОПАП ЗАКРОЕТСЯ
-    item.addEventListener('mousedown', (evt) => { // ВЕШАЕМ СЛУШАТЕЛЬ КЛИКА НА ОВЕРЛЕЙ
-      if (evt.target.classList.contains('popup_opened')) { // ЕСЛИ ЭТО ОВЕРЛЕЙ ПРОФАЙЛА
-        closePopup(item); // ОТПРАВИЛИ ФОРМУ ПРОФАЙЛА В ФУНКЦИЮ-ОЧИЩАТЕЛЬ ФОРМ
-      }
-    });
-  });
-}
-
-closeOverlay();
 
 export { closePopupByEsc, closePopup };
